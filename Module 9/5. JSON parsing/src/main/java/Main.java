fa3994de02ebda70aa09adff03420099d1446786
Rinzler <rinz.ler@aiesec.net>
2020-01-19 17:34:28 +0300

    advanced HTML parsing added

--- Module 9/5. JSON parsing/src/main/java/Main.java
+++ Module 9/5. JSON parsing/src/main/java/Main.java
@@ -1,26 +1,43 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
 
 public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        Document document = Jsoup.connect("https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0").maxBodySize(0).get();
-        Element table = document.select("table").get(3);
+        ///////////////////////////////////////////////Создание JSON файла//////////////////////////////////////////////
+        JSONObject moscowMetroMap = new JSONObject();
+        //moscowMetroMap.put("stations", );
+        //moscowMetroMap.put("lines", );
+        //в moscowMetroMap положим станции и линии
+        JSONObject stations = new JSONObject();
+        JSONArray arrayStations = new JSONArray();
 
-        Elements lines = table.select("tr");
-        lines.forEach(System.out::println);
-        lines.stream().skip(1).forEach(line ->
-        {
-            Elements elements = line.select("td");
-            String stationName = elements.get(1).text();
-            String lineName = elements.get(0).child(1).attr("title");  //child() - подпункты выбранного элемента. 1 строка = 1 подпункт
-            System.out.println("Станция: " + stationName + "; Линия: " + lineName);
+        TreeMap<String, String> stationsByLineNumber = new TreeMap<>();
+
+        String previousLineNumber = "1";
+        ///////////////////////////////////////////Парсинг HTML с метро/////////////////////////////////////////////////
+        Document document = Jsoup.connect("https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0").maxBodySize(0).get();
+        Element table = document.select("table:has(span a[title='Бульвар Рокоссовского (станция метро)'])").first();
+        Elements rows = table.select("tr:nth-of-type(1n+2)"); //пропуск первого tr с названиями колонок таблицы
+        rows.stream().forEach(row -> {
+            String stationName = row.select("td:nth-child(2)").first().text();
+            String lineName = row.select("td:nth-child(1) span").attr("title");
+            String lineNumber = row.select("td:nth-child(1)").attr("data-sort-value");
+           // System.out.println("Название станции: " + stationName + "; Название линии: " + lineName + "; Номер линии: " + lineNumber);
+            stationsByLineNumber.put(lineNumber, stationName);
+
         });
+        System.out.println(stationsByLineNumber);
 
     }
 }
